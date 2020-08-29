3126d68f6dc35d0c355dec47304a6d3ebdfe044f
StasyaV <stasya522@mail.ru>
2020-08-29 19:42:55 +0300

    Линтер

--- src/utils/film.js
+++ src/utils/film.js
@@ -2,22 +2,6 @@ export const formatDate = (date) => {
   return date.toLocaleDateString(`en-GB`, {day: `numeric`, month: `long`, year: `numeric`});
 };
 
-const getWeightForNullDate = (dateA, dateB) => {
-  if (dateA === null && dateB === null) {
-    return 0;
-  }
-
-  if (dateA === null) {
-    return 1;
-  }
-
-  if (dateB === null) {
-    return -1;
-  }
-
-  return null;
-};
-
 export const sortFilmByDate = (filmA, filmB) => {
   const dateA = new Date(filmA.releaseDay);
   const dateB = new Date(filmB.releaseDay);
